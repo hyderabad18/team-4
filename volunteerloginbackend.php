50b31dc95cab553e38ff213d0ca3ed4b1697fa57
sushmalakhinena <lakhinenasushmasree@gmail.com>
2018-07-14 20:45:37 +0530

    admin login backend and volunteer login backend commit

--- volunteerloginbackend.php
+++ volunteerloginbackend.php
@@ -2,24 +2,25 @@
 require_once ("../bridge/connection.php");
 $conn = main_db_connect();
 
+
 // If form submitted, insert values into the database.
-if (isset($_POST['username'])){
+if (isset($_POST['name'])){
         // removes backslashes
-	$username = stripslashes($_REQUEST['username']);
+	$name = stripslashes($_REQUEST['name']);
         //escapes special characters in a string
-	$username = mysqli_real_escape_string($con,$username);
+	$name = mysqli_real_escape_string($con,$name);
 	$password = stripslashes($_REQUEST['password']);
 	$password = mysqli_real_escape_string($con,$password);
 	$hashed= hash('sha512',$password);
 	//Checking is user existing in the database or not
-        $query = "SELECT * FROM `admin` WHERE username='$username'
+        $query = "SELECT * FROM `volunteer_reg` WHERE username='$name'
 and password='$hashed'";
 	$result = mysqli_query($con,$query) or die(mysql_error());
 	$rows = mysqli_num_rows($result);
         if($rows==1){
+			echo 'login sucessfull!!';
 	   // $_SESSION['username'] = $username;
             // Redirect user to index.php
-			echo 'login sucessfull!!';
 	    header("Location: index.php");
          }else{
 	echo "<div class='form'>
