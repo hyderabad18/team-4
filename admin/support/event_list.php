168e0bb60d592c48135f9b61d052fbd2e910690c
ModuguNikhitha <35829465+ModuguNikhitha@users.noreply.github.com>
2018-07-14 22:31:35 +0530

    event_list
    indenting

--- admin/support/event_list.php
+++ admin/support/event_list.php
@@ -54,21 +54,22 @@ input[type=submit]:hover {
   <div class="panel panel-default">
     <div class="panel-body">
     <h4>Description</h4>
-Chiguru is a mega event that allows children from government schools and other needy shelters to display their cultural and literary talents. 
+    Chiguru is a mega event that allows children from government schools and other needy shelters to display their cultural and literary talents. 
 
-<h4>Location of the event</h4>
-    Mehdipatnam,Kukkatpally,Secundrabad</div>
+    <h4>Location of the event</h4>
+    Mehdipatnam,Kukkatpally,Secundrabad
     <h4>Start Date</h4>20-07-2018
-    <h4>End Date</h4>21-07-2018
+    <h4>End Date</h4>21-07-2018</div>
   </div>
   <h2>Spoorthi</h2>
   <div class="panel panel-default">
-<h4>Description</h4>
-    <div class="panel-body">A platform for children with special abilities to exhibit their talents. 
+    <div class="panel-body">
+    	<h4>Description</h4>A platform for children with special abilities to exhibit their talents. 
 <h4>Location of the event</h4> 
-Dilsuknagar,Uppal,Secundrabad</div> 
+Dilsuknagar,Uppal,Secundrabad 
 <h4>Start Date</h4>20-08-2018
 <h4>End Date</h4>29-08-2018 	
+    </div>
     </div>
   </div>
   <h2>Audio Recording for specially abled</h2>
@@ -77,9 +78,10 @@ Dilsuknagar,Uppal,Secundrabad</div>
     <h4>Description</h4>
 Audio recording of textbooks for the visually challenged is a popular virtual volunteering opportunity, with volunteers contributing audio recordings from their homes and/or offices.
 <h4>Location of the event</h4> 
-Nagole,Secundrabad</div>
+Nagole,Secundrabad
 <h4>Start Date</h4>15-09-2018
  <h4>End Date</h4>15-09-2018
+ </div>
 </div>
   </div>
 
@@ -89,9 +91,10 @@ Nagole,Secundrabad</div>
     <h4>Description</h4>
 National Means-cum-Merit Scholarship scheme (NMMS) is a centrally sponsored scheme, to award scholarships to meritorious students hailing from low income backgrounds. The motive behind this scheme is to encourage students to continue their education and thus minimize secondary school drop outs.
 <h4>Location of the event</h4> 
-Nagole,Panjagutta</div>
+Nagole,Panjagutta
 <h4>Start Date</h4>15-07-2018
  <h4>End Date</h4>15-09-2018
 </div>
   </div>
+</div>
 </div>
