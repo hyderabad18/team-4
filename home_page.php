0d04143c417fa8edb6e31ab643dfa792745c743a
joan8ruth <njoanna8@gmail.com>
2018-07-15 03:34:22 +0530

    Home_Page
    home page header only

---
+++ home_page.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
+	<meta name="viewport" content="width=device-width, user-scalable=no" />
+	<meta name="Keywords" content="OUCE,UCEOU, Placement, recruit">
+
+	<title>Youth for Seva</title>
+
+	<link rel="stylesheet" href="/library/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/library/css/stylesheet.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" crossorigin="anonymous">
+
+
+
+	<!-- <link href="assets/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css" media="screen"/> -->
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	      <script src="../../assets/js/html5shiv.js"></script>
+	      <script src="../../assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+
+<div class="container-fluid navbar box-shadow">
+  <div class="container-fluid" style="max-width: 1200px;">
+      
+    <div class="sm-6 text-left">
+        <h2 class="menu-txt">Youth for SEVA</h2>
+    </div>
+    <div class="sm-3">
+      <button type="button" class="btn btn-outline-light">Admin Login</button>
+      <button type="button" class="btn btn-outline-light">Volunteer Login</button>
+      <button type="button" class="btn btn-outline-light">Volunteer Register</button>
+
+    </div>
+  </div>  
+</div>
+</body>
+</html>
