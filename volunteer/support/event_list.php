d1f8e60da25d30bdc6840c5cbd8d937146b9f589
MSSR-code <mssr97@gmail.com>
2018-07-14 20:21:59 +0530

    volunteer initial update

---
+++ volunteer/support/event_list.php
@@ -0,0 +1,97 @@
+<style>
+input[type=text], select
+ {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+#content {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+</style>
+<title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+  <h2>Vidyachetana-Sponser a child </h2>
+  <div class="panel panel-default">
+    <div class="panel-body">
+    	<h4>Description</h4>
+    Vidyachetana provides educational support to children from underprivileged backgrounds. Vidyachetana is carved out with the intention to create community outreach centres assisting in the fields of education, and vocational training in economically backward areas.
+    <h4>Location of the event</h4>
+    LBnagar,Kukkatpally,Secundrabad
+    <h4>Start Date</h4>15-07-2018
+    <h4>End Date</h4>15-08-2018
+     </div>
+  </div>
+  <h2>Chiguru event</h2>
+  <div class="panel panel-default">
+    <div class="panel-body">
+    <h4>Description</h4>
+Chiguru is a mega event that allows children from government schools and other needy shelters to display their cultural and literary talents. 
+
+<h4>Location of the event</h4>
+    Mehdipatnam,Kukkatpally,Secundrabad</div>
+    <h4>Start Date</h4>20-07-2018
+    <h4>End Date</h4>21-07-2018
+  </div>
+  <h2>Spoorthi</h2>
+  <div class="panel panel-default">
+<h4>Description</h4>
+    <div class="panel-body">A platform for children with special abilities to exhibit their talents. 
+<h4>Location of the event</h4> 
+Dilsuknagar,Uppal,Secundrabad</div> 
+<h4>Start Date</h4>20-08-2018
+<h4>End Date</h4>29-08-2018 	
+    </div>
+  </div>
+  <h2>Audio Recording for specially abled</h2>
+  <div class="panel panel-default">
+    <div class="panel-body">
+    <h4>Description</h4>
+Audio recording of textbooks for the visually challenged is a popular virtual volunteering opportunity, with volunteers contributing audio recordings from their homes and/or offices.
+<h4>Location of the event</h4> 
+Nagole,Secundrabad</div>
+<h4>Start Date</h4>15-09-2018
+ <h4>End Date</h4>15-09-2018
+</div>
+  </div>
+
+   <h2>NMMS</h2>
+  <div class="panel panel-default">
+    <div class="panel-body">
+    <h4>Description</h4>
+National Means-cum-Merit Scholarship scheme (NMMS) is a centrally sponsored scheme, to award scholarships to meritorious students hailing from low income backgrounds. The motive behind this scheme is to encourage students to continue their education and thus minimize secondary school drop outs.
+<h4>Location of the event</h4> 
+Nagole,Panjagutta</div>
+<h4>Start Date</h4>15-07-2018
+ <h4>End Date</h4>15-09-2018
+</div>
+  </div>
+</div>
---
+++ volunteer/support/volunteer_registration.php
@@ -0,0 +1,61 @@
+<style>
+input[type=text], select
+ {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+
+#heading{
+
+    
+}
+#form-layout {
+    border-radius: 5px;
+    background-color: #f2f2f2;
+    padding: 20px;
+}
+</style>
+
+<div id="form-layout">
+  <form action="/action_page.php">
+    <label for="ename"> Name</label>
+    <input type="text" id="ename" name="name" placeholder="event name..">
+
+    <label for="edesc">Contact</label>
+    <input type="text" id="lname" name="contact" placeholder="Contact">
+
+    <label for="edesc">Email Id</label>
+    <input type="text" id="lname" name="email" placeholder="Email Id">
+
+    <label for="edesc">Location</label>
+    <input type="text" id="lname" name="location" placeholder="Age">
+
+    <label for="edesc">Profession</label>
+    <input type="text" id="lname" name="profession" placeholder="Profession">
+
+    <label for="edesc">Pass Key</label>
+    <input type="text" id="lname" name="password" placeholder="Pass Key">
+
+    
+  
+    <input type="submit" value="Submit">
+  </form>
+</div>
