353d41cec45f0ebf07bfdc3549ec10a7bf6a320c
ModuguNikhitha <35829465+ModuguNikhitha@users.noreply.github.com>
2018-07-15 01:07:43 +0530

    volunteer_list
    volunteer list entry

---
+++ admin/support/volunteer_list.php
@@ -0,0 +1,30 @@
+<style>
+#notice {
+    max-width: 1200px;
+    margin:0px auto;
+}
+
+    
+
+</style>
+
+<div id="notice">
+	 <?php 
+?>    
+  
+
+  <div class="widget">
+    <div class="widget-head">
+        <h5>List of the Volunteers</h5>
+    </div>
+    
+        
+
+    </div>
+  </div>
+
+</a>
+<?php ?>
+
+
+</div>
