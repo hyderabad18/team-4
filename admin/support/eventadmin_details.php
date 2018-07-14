66b1bb03e44290c59cb5b80f79221cf8a7f1de93
sushmalakhinena <lakhinenasushmasree@gmail.com>
2018-07-15 05:02:41 +0530

    event details admin and volunteer commit

---
+++ admin/support/eventadmin_details.php
@@ -0,0 +1,49 @@
+<style>
+#notice {
+    max-width: 1200px;
+    margin:0px auto;
+}
+</style>
+
+<div id="notice">
+
+  <div class="widget">
+    <div class="widget-head">
+        <h5>Vidyachetana-Sponser a child </h5>
+    </div>
+    <div class="widget-content">
+        <div class="widget-description d-none d-sm-block">
+            <p>Don't worry. If you don't make any changes here, nothing will go wrong.
+                At this point you can examine the content of the files and see if the application works as explained in the articles.
+              If you make changes to the files you can revert them by typing.Don't worry. If you don't make any changes here, nothing will go wrong.
+                At this point you can examine the content of the files and see if the application works as explained in the articles.
+              If you make changes to the files you can revert them by typing</p>
+        </div>
+        <div class="row">
+            <div class="widget-location col-12 col-sm-4">Kukatpally</div>
+            <div class="widget-time col-12 col-sm-8">
+                <div class="time-from col-5">15-07-2018</div>
+                <div class="time-from col-2">To</div>
+                <div class="time-to col-5">15-08-2018</div>
+            </div>
+        </div>
+        <br>
+        <br>
+       
+
+    </div>
+  </div>
+<div class="widget">
+    <div class="widget-head">
+        <h5>List of the Volunteers</h5>
+        <?php for ($i=0; $i < 5; $i++) {
+                echo "Name $i"; 
+        ?>
+        <br>
+    <?php } ?>
+    
+
+
+    </div>
+
+</div>
