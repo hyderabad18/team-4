6a65284aa406b7c8d9c23c765f0e7a267119298e
MSSR-code <mssr97@gmail.com>
2018-07-15 07:24:18 +0530

    dynamic UI admin

--- admin/support/event_details.php
+++ admin/support/event_details.php
@@ -1,30 +1,48 @@
+<?php 
+
+  require_once ($_SERVER['DOCUMENT_ROOT']."/bridge/connection.php");
+  $conn = main_db_connect();
+
+  if (isset($_GET['event_id'])) 
+  {
+      $id=mysqli_real_escape_string($conn,addslashes($_GET['event_id']));
+      $sql = "SELECT `event_name`, `description`, `category`, `location`, `start_date`, `end_date`, `volunteers_required`, `timestamp`, `comments` FROM `event_entry` WHERE `event_id` = '$id'" ;
+      $res = $conn->query($sql)->fetch_assoc();
+  }
+
+ ?>
+
+
+
 <style>
 #notice {
     max-width: 1200px;
     margin:0px auto;
 }
+.widget-row
+{
+    padding:5px;
+    border-bottom: 2px solid #BDBDBD;
+}
+
 </style>
 
 <div id="notice">
 
   <div class="widget">
     <div class="widget-head">
-        <h5>Vidyachetana-Sponser a child </h5>
+        <h5><?php echo $res['event_name']; ?></h5>
     </div>
     <div class="widget-content">
         <div class="widget-description d-none d-sm-block">
-            <p>Don't worry. If you don't make any changes here, nothing will go wrong.
-                At this point you can examine the content of the files and see if the application works as explained in the articles.
-              If you make changes to the files you can revert them by typing.Don't worry. If you don't make any changes here, nothing will go wrong.
-                At this point you can examine the content of the files and see if the application works as explained in the articles.
-              If you make changes to the files you can revert them by typing</p>
+            <p><?php echo $res['description']; ?></p>
         </div>
         <div class="row">
             <div class="widget-location col-12 col-sm-4">Kukatpally</div>
             <div class="widget-time col-12 col-sm-8">
-                <div class="time-from col-5">15-07-2018</div>
+                <div class="time-from col-5"><?php echo $res['start_date']; ?></div>
                 <div class="time-from col-2">To</div>
-                <div class="time-to col-5">15-08-2018</div>
+                <div class="time-to col-5"><?php echo $res['end_date']; ?></div>
             </div>
         </div>
         <br>
@@ -36,14 +54,41 @@
 <div class="widget">
     <div class="widget-head">
         <h5>List of the Volunteers</h5>
-        <?php for ($i=0; $i < 5; $i++) {
-                echo "Name $i"; 
-        ?>
-        <br>
-    <?php } ?>
-    
+    </div>
+
+<div class="widget-content">
+
+    <?php 
+
+    $sql = "SELECT `v_id` FROM `applied_volunteers` WHERE `event_id` = '$id'";
+
+    $res = $conn->query($sql);
+
+    while ($row = $res->fetch_assoc()) 
+    {
+        $sql = "SELECT `name` , `contact` , `email` FROM `volunteer_reg` WHERE  `v_id` = '".$row['v_id']."'";
+        $vol = $conn->query($sql)->fetch_assoc();
+?>
+
+    <div class="row widget-row">
+                <div class="col-8 row text-center">
+                    <div class="widget-name col-4"><?php echo $vol['name']; ?></div>
+                    <div class="widget-name col-4 d-none d-sm-block"><?php echo $vol['contact']; ?></div>
+                    <div class="widget-name col-4 d-none d-md-block"><?php echo $vol['email']; ?></div>
+                </div>
+                <div class="col-4 text-center">
+                    <button class="accept btn btn-outline-success">ACCEPT</button>
+                </div>
+        </div>
 
 
+
+<?php
+
+    }
+
+     ?>
+        
     </div>
 
 </div>
--- admin/support/event_list.php
+++ admin/support/event_list.php
@@ -1,3 +1,14 @@
+<?php 
+
+  require_once ($_SERVER['DOCUMENT_ROOT']."/bridge/connection.php");
+  $conn = main_db_connect();
+
+      $sql = "SELECT `event_id`, `event_name`, `description`, `location`, `start_date`, `end_date` FROM `event_entry` WHERE `start_date` >= NOW()";
+      $res = $conn->query($sql);
+
+
+ ?>
+
 <style>
 #notice {
     max-width: 1200px;
@@ -12,27 +23,27 @@ a {
 
 <div id="notice">
 
-  <?php for ($i=0; $i < 5; $i++) { 
+  <?php while($row = $res->fetch_assoc())
+  {
+
 ?>    
   
 
-<a href="index?route=event_details&event_id=2">
+<a href="index?route=event_details&event_id=<?php echo $row['event_id']; ?>">
   <div class="widget">
     <div class="widget-head">
-        <h5>Vidyachetana-Sponser a child </h5>
+        <h5><?php echo $row['event_name']; ?></h5>
     </div>
     <div class="widget-content">
         <div class="widget-description d-none d-sm-block">
-            <p>Don't worry. If you don't make any changes here, nothing will go wrong.
-                At this point you can examine the content of the files and see if the application works as explained in the articles.
-              If you make changes to the files you can revert them by typing</p>
+            <p><?php echo $row['description']; ?></p>
         </div>
         <div class="row">
-            <div class="widget-location col-12 col-sm-4">Kukatpally</div>
+            <div class="widget-location col-12 col-sm-4"><?php echo $row['location']; ?></div>
             <div class="widget-time col-12 col-sm-8">
-                <div class="time-from col-5">15-07-2018</div>
+                <div class="time-from col-5"><?php echo $row['start_date']; ?></div>
                 <div class="time-from col-2">To</div>
-                <div class="time-to col-5">15-08-2018</div>
+                <div class="time-to col-5"><?php echo $row['end_date']; ?></div>
             </div>
         </div>
 
